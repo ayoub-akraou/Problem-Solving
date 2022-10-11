c3d1aca54958a491629278c5eef0ae718e8c09e1
unknown <ayoubakraou@gmail.com>
2022-10-11 19:16:29 +0100

    problem 4

---
+++ A wolf in sheep's clothing/Description.js
@@ -0,0 +1,9 @@
+function warnTheSheep(queue) {
+  const wolfIndex = queue.indexOf("wolf") + 1;
+  if (queue[queue.length - 1] === "wolf") {
+    return "Pls go away and stop eating my sheep";
+  }
+  return `Oi! Sheep number ${
+    queue.length - wolfIndex
+    }! You are about to be eaten by a wolf!`;
+}
