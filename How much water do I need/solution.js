ac196952e3ad9f444acda203fe355e18bee86b1b
Ayoub Akraou <ayoubakraou@gmail.com>
2022-11-08 20:55:50 +0100

    new

---
+++ How much water do I need/solution.js
@@ -0,0 +1,6 @@
+function howMuchWater(water, load, clothes){
+  if (clothes > 2 * load) return 'Too much clothes';
+  if (clothes < load) return 'Not enough clothes';
+  
+  return +((water * 1.1 ** (clothes - load)).toFixed(2));
+}
